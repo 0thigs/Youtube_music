dee04458e4df6b899e5e1e3f13b6a3e11033cf8f
0thigs <machinimavlogs@gmail.com>
2024-04-25 00:06:56 -0300

    🌟first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+template_dir = "./src/templates"
+static_dir = "./src/static"
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
