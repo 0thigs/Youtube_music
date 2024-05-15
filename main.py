600a5ccc02e1701668bb9d20075b876aea5fa685
0thigs <machinimavlogs@gmail.com>
2024-05-14 23:29:22 -0300

    ⚙ config: add mysql connector config

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template
+import mysql.connector
+
+config = {
+    'host': 'database',
+    'port': 3306,
+    'user': 'thigs',
+    'password': 'root',
+    'database': 'youtube',
+}
+
+database = mysql.connector.connect(**config)
 
 template_dir = "./src/templates"
 static_dir = "./src/static"
@@ -10,5 +21,5 @@ def homepage():
     return render_template('homepage.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
         
