33239469ca1c1d2efbe0753afce4e35cfdbcfd7e
0thigs <machinimavlogs@gmail.com>
2024-05-21 22:11:00 -0300

    🖥scripts: post and get from database

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from pathlib import Path
+from random import randint
+from uuid import uuid4
+
+from flask import Flask, request, render_template
 import mysql.connector
 
 config = {
@@ -10,16 +14,41 @@ config = {
 }
 
 database = mysql.connector.connect(**config)
-cursor = database.cursor()
+cursor = database.cursor(dictionary=True)
 
 template_dir = "./src/templates"
 static_dir = "./src/static"
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def homepage():
-    return render_template('homepage.html')
+    if request.method == "POST":
+        print(request.form, flush=True)
+
+        img_music = request.files.get("img_music")
+        print(img_music, flush=True)
+
+        image_name = f"{uuid4()}.jpg"
+        image_path = Path().absolute() / "src" / "static" / "uploads" / image_name
+
+        img_music.save(image_path)
+
+        title = request.form.get("title")
+        description = request.form.get("description")
+        url_music = request.form.get("url_music")
+
+        query = f"INSERT INTO post(titulo, description ,musicurl, musicimage) VALUES ('{title}', '{description}' ,'{url_music}', '{image_name}')"
+        cursor.execute(query)
+        database.commit()
+
+    get_query = f"SELECT * FROM post"
+    cursor.execute(get_query)
+    musics = cursor.fetchall()
+
+    print(musics)
+
+    return render_template('homepage.html', musics=musics)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
