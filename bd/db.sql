8f2f19b381acd89104940adb7e85ea49ff90b291
0thigs <machinimavlogs@gmail.com>
2024-05-21 22:11:32 -0300

    💾database: database update

--- bd/db.sql
+++ bd/db.sql
@@ -1,22 +1,16 @@
--- Active: 1715783627262@@localhost@3307
+-- Active: 1715737537685@@127.0.0.1@3307@youtube
 use youtube
-
-create table user(
-    id int primary key auto_increment,
-    nome varchar(50) not null,
-    email varchar(50) not null,
-    senha varchar(50) not null
-)
 
 create table post (
     id int primary key auto_increment,
     titulo varchar(50) not null,
+    description text not null,
     musicurl varchar(250) not null,
-    image varchar(50) not null
+    musicimage VARCHAR(250) not null
 )
+
+INSERT INTO post(titulo, description ,musicurl, musicimage) VALUES ("Teste", "Neina é gay" ,"https://www.youtube.com/watch?v=lA2L6qHuBeg", "banana.png")
 
 drop table post
 
-drop table user
-
-SHOW TABLES
+SELECT * from post
